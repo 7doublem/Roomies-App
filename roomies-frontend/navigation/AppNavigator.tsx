6c974c1eaa8c457c8cd9365c0ba4eb8634c8a6eb
Wai Hleng Swe <waihlengpyae@gmail.com>
2025-06-05 14:48:54 +0100

    feat!: Improved styling of the signinscreen and the signupscreen

--- roomies-frontend/navigation/AppNavigator.tsx
+++ roomies-frontend/navigation/AppNavigator.tsx
@@ -116,7 +116,7 @@ export default function AppNavigator() {
   return (
     <NavigationContainer>
       {/* {isSignedIn ? <AppTabs /> : <AuthStack />} */}
-      <AppTabs/>
+      <AuthStack/>
     </NavigationContainer>
   );
 }
