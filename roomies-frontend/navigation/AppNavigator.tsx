b90d8daf31b1e4efd2c134d2c92439acec9efccc
Wai Hleng Swe <waihlengpyae@gmail.com>
2025-06-06 16:26:48 +0100

    Update AppNavigator.tsx

--- roomies-frontend/navigation/AppNavigator.tsx
+++ roomies-frontend/navigation/AppNavigator.tsx
@@ -117,7 +117,7 @@ export default function AppNavigator() {
   return (
     <NavigationContainer>
       {/* {isSignedIn ? <AppTabs /> : <AuthStack />} */}
-      <AuthStack/>
+      <AppTabs/>
     </NavigationContainer>
   );
 }
