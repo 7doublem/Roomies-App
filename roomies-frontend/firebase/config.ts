2d4f1e50d9ce1033f49ab26a8347f7767266cad5
Wai Hleng Swe <waihlengpyae@gmail.com>
2025-06-09 15:51:18 +0100

    feat: Reduced security dependencies

--- roomies-frontend/firebase/config.ts
+++ roomies-frontend/firebase/config.ts
@@ -3,22 +3,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
+import firebaseConfig from './firebaseConfig';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAWW5RTBkvBIjAAWgJ1wF0_qhDLuLN1r6M",
-  authDomain: "roomies-app-32362.firebaseapp.com",
-  projectId: "roomies-app-32362",
-  storageBucket: "roomies-app-32362.firebasestorage.app",
-  messagingSenderId: "434083306699",
-  appId: "1:434083306699:web:98701bd7dbe383832b4f4e",
-  measurementId: "G-ZV9RXJ8GBX"
-};
 
-// Initialize Firebase
+// Initialise Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
