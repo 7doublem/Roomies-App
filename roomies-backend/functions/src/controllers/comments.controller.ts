3a3a432b6f4ae94205018a04de3bad404a32352c
R3VLver9999 <tatkinson1902@gmail.com>
2025-06-09 16:29:20 +0100

    Completed Delete comments endpoint

--- roomies-backend/functions/src/controllers/comments.controller.ts
+++ roomies-backend/functions/src/controllers/comments.controller.ts
@@ -1,30 +1,29 @@
-import {Request, Response, NextFunction} from "express";
-import {getFirestore, Timestamp} from "firebase-admin/firestore";
-
+import { Request, Response, NextFunction } from "express";
+import { getFirestore, Timestamp } from "firebase-admin/firestore";
 type Comment = {
   // commentId:string,
-  choreId: string,
-  commentBody: string,
-  createdAt: number,
-  createdBy: string, // uid but show name for the front....
+  choreId: string;
+  commentBody: string;
+  createdAt: number;
+  createdBy: string; // uid but show name for the front....
 };
 
 export class commentController {
   // POST /chores/:chore_id/comments - creator of comments for a chore
   static async createComment(req: Request, res: Response, next: NextFunction) {
     try {
-      const {commentBody} = req.body;
+      const { commentBody } = req.body;
       const creatorUid = req.user?.uid;
       const groupId = req.params.group_id;
       const choreId = req.params.chore_id;
 
       if (!creatorUid) {
-        res.status(401).json({message: "Unauthorised"});
+        res.status(401).json({ message: "Unauthorised" });
         return;
       }
 
       if (!commentBody) {
-        res.status(400).json({message: "Commment's body is required"});
+        res.status(400).json({ message: "Commment's body is required" });
         return;
       }
 
@@ -32,7 +31,7 @@ export class commentController {
       const groupDoc = await groupRef.get();
 
       if (!groupDoc.exists) {
-        res.status(404).send({message: "Group not found"});
+        res.status(404).send({ message: "Group not found" });
         return;
       }
 
@@ -40,7 +39,7 @@ export class commentController {
       const choreDoc = await choreRef.get();
 
       if (!choreDoc.exists) {
-        res.status(404).json({message: "Chore not found"});
+        res.status(404).json({ message: "Chore not found" });
         return;
       }
 
@@ -53,16 +52,19 @@ export class commentController {
       };
 
       const commentRef = await choreRef.collection("comments").add(newComment);
-      res.status(201).json({commentId: commentRef.id, ...newComment});
+      res.status(201).json({ commentId: commentRef.id, ...newComment });
       return;
     } catch (error) {
-      console.error(error);
       next(error);
     }
   }
 
   // GET /chores/:chore_id/comments - get all comments related to an specific chore
-  static async getAllCommentsByChoreId(req: Request, res: Response, next: NextFunction) {
+  static async getAllCommentsByChoreId(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
     try {
       const groupId = req.params.group_id;
       const choreId = req.params.chore_id;
@@ -71,7 +73,7 @@ export class commentController {
       const groupDoc = await groupRef.get();
 
       if (!groupDoc.exists) {
-        res.status(404).send({message: "Group not found"});
+        res.status(404).send({ message: "Group not found" });
         return;
       }
 
@@ -79,7 +81,7 @@ export class commentController {
       const choreDoc = await choreRef.get();
 
       if (!choreDoc.exists) {
-        res.status(404).json({message: "Chore not found"});
+        res.status(404).json({ message: "Chore not found" });
         return;
       }
 
@@ -93,7 +95,104 @@ export class commentController {
       res.status(200).json(comments);
       return;
     } catch (error) {
-      console.error(error);
+      next(error);
+    }
+  }
+  // GET /chores/:chore_id/comment/:comment_id - get a single comment related to a chore by it's id
+  static async getCommentByCommentId(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    try {
+      const groupId = req.params.group_id;
+      const choreId = req.params.chore_id;
+      const commentId = req.params.comment_id;
+
+      const groupRef = await getFirestore().collection("groups").doc(groupId);
+      const groupDoc = await groupRef.get();
+
+      if (!groupDoc.exists) {
+        res.status(404).send({ message: "Group not found" });
+        return;
+      }
+
+      const choreRef = groupRef.collection("chores").doc(choreId);
+      const choreDoc = await choreRef.get();
+
+      if (!choreDoc.exists) {
+        res.status(404).json({ message: "Chore not found" });
+        return;
+      }
+
+      const commentRef = choreRef.collection("comments").doc(commentId);
+      const commentDoc = await commentRef.get();
+
+      if (!commentDoc.exists) {
+        res.status(404).json({ message: "Comment not found" });
+        return;
+      }
+
+      const comment = await getFirestore()
+        .collection("groups")
+        .doc(groupId)
+        .collection("chores")
+        .doc(choreId)
+        .collection("comments")
+        .doc(commentId)
+        .get();
+      res.status(200).json(comment);
+      return;
+    } catch (error) {
+      next(error);
+    }
+  }
+  // DELETE /chores/:chore_id/comments/:comment_id - get all comments related to an specific chore
+  static async deleteCommentById(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    try {
+      const groupId = req.params.group_id;
+      const choreId = req.params.chore_id;
+      const commentId = req.params.comment_id;
+
+      const groupRef = await getFirestore().collection("groups").doc(groupId);
+      const groupDoc = await groupRef.get();
+
+      if (!groupDoc.exists) {
+        res.status(404).send({ message: "Group not found" });
+        return;
+      }
+
+      const choreRef = groupRef.collection("chores").doc(choreId);
+      const choreDoc = await choreRef.get();
+
+      if (!choreDoc.exists) {
+        res.status(404).json({ message: "Chore not found" });
+        return;
+      }
+
+      const commentRef = choreRef.collection("comments").doc(commentId);
+      const commentDoc = await commentRef.get();
+
+      if (!commentDoc.exists) {
+        res.status(404).json({ message: "Comment not found" });
+        return;
+      }
+
+      await getFirestore()
+        .collection("groups")
+        .doc(groupId)
+        .collection("chores")
+        .doc(choreId)
+        .collection("comments")
+        .doc(commentId)
+        .delete();
+      res.status(204)
+      return;
+    } catch (error) {
       next(error);
     }
   }
