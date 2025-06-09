001c8e3f757a20ac525c7bef6c772bf66d6a00fa
7doublem <mustafamoallim@gmail.com>
2025-06-09 22:01:37 +0100

    using default service account env variables for prod

--- roomies-backend/functions/src/app.ts
+++ roomies-backend/functions/src/app.ts
@@ -10,31 +10,31 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const isEmulator = process.env.FUNCTIONS_EMULATOR === "true" || process.env.FIRESTORE_EMULATOR_HOST;
-
 if (!admin.apps.length) {
+  const isEmulator =
+    process.env.FUNCTIONS_EMULATOR === "true" || !!process.env.FIRESTORE_EMULATOR_HOST;
+
   if (isEmulator) {
     process.env.FIRESTORE_EMULATOR_HOST ||= "127.0.0.1:8080";
     process.env.FIREBASE_AUTH_EMULATOR_HOST ||= "127.0.0.1:9099";
     console.log("Using Firestore and Auth Emulators");
 
     admin.initializeApp({projectId: "roomies-app-32362"});
-  } else {
-    const projectId = process.env.FIREBASE_PROJECT_ID;
-    const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
-    const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n");
-
-    if (!projectId || !clientEmail || !privateKey) {
-      throw new Error("Missing Firebase service account environment variables");
-    }
-
+  } else if (
+    process.env.FIREBASE_PROJECT_ID &&
+    process.env.FIREBASE_CLIENT_EMAIL &&
+    process.env.FIREBASE_PRIVATE_KEY
+  ) {
     admin.initializeApp({
       credential: admin.credential.cert({
-        projectId,
-        clientEmail,
-        privateKey,
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
       }),
     });
+  } else {
+    console.log("Using application default credentials");
+    admin.initializeApp();
   }
 }
 
